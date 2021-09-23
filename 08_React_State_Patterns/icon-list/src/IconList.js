572e7712661e43c1cb79b4f1b14a366e6061ad02
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-09-23 15:37:46 +0100

    used map, spread old array into new array with new icon

--- 08_React_State_Patterns/icon-list/src/IconList.js
+++ 08_React_State_Patterns/icon-list/src/IconList.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react'
 
 class IconList extends Component {
-    constructor(props){
+    static defaultProps = {
+        options: [
+            'surprise',
+            'sad-tear',
+            'grin-wink',
+            'laugh',
+            'grimace',
+            'frown',
+            'dizzy'
+        ]
+    }
+    constructor(props) {
         super(props);
-        this.state = 
+        this.state = { icons: [] }
+        this.addIcon = this.addIcon.bind(this);
     }
-    
-    
-    render(){
-        return(
 
-        )
+    addIcon() {
+        let idx = Math.floor(Math.random() * this.props.options.length)
+        let newIcon = this.props.options[idx];
+        this.setState({ icons: [...this.state.icons, newIcon] });
+        // put old array into an array with new icon
     }
-    
+
+    render() {
+        const icons = this.state.icons.map(i => <i className={`fas fa-${i}`} />);
+
+
+        return (
+            <div>
+                <h1> Icons: {icons} </h1>
+                <button onClick={this.addIcon}>Add Icon</button>
+            </div>
+        )
+        // add icons to h1 when you click on button
+    }
+
 }
+
+export default IconList;

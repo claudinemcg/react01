8f723b08f1a0449b3885e513404bc035f7a5328f
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-09-24 12:26:41 +0100

    coded num for numBalls and maxNum props and added styling

--- 08_React_State_Patterns/lottery/src/App.js
+++ 08_React_State_Patterns/lottery/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Lottery />
+      <Lottery title='Mini Daily' numBalls={4} maxNum={25} />
     </div>
   );
 }
--- 08_React_State_Patterns/lottery/src/Lottery.js
+++ 08_React_State_Patterns/lottery/src/Lottery.js
@@ -10,28 +10,34 @@ class Lottery extends Component {
     }
     constructor(props) {
         super(props);
-        this.state = { nums: Array.from({ length: this.props.maxBalls }) };
+        this.state = { nums: Array.from({ length: this.props.numBalls }) };
+        // creates new array the length of the numBalls prop
         this.handleClick = this.handleClick.bind(this);
     }
 
     generate() {
-
+        this.setState(currState => ({
+            // currState is callback
+            nums: currState.nums.map(
+                n => Math.floor(Math.random() * this.props.maxNum) + 1
+                // want it to be 1 -40 would give 0 - 39 without +1
+            )
+        }));
     }
-
     handleClick() {
         this.generate();
     }
 
     render() {
         return (
-            <section>
+            <section className="Lottery">
                 <h1>{this.props.title}</h1>
                 <div>
                     {this.state.nums.map(n => <LottoBall num={n} />)}
                 </div>
                 <button onClick={this.handleClick}>Generate</button>
 
-            </section>
+            </section >
         )
     }
 }

4d0f2266535599e023f7019a7ba2a562864987e9
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-11-30 17:28:24 +0000

    added multiple forms, changed handleChange to use [evt.target.name] for multiple changes

--- 13_React_Forms/first_form/src/App.js
+++ 13_React_Forms/first_form/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Form from "./Form";
+import MultipleForm from "./MultipleForm";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Form />
+        <MultipleForm />
       </div>
     )
   }
---
+++ 13_React_Forms/first_form/src/MultipleForm.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { username: '', email:'', password:'' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(evt) {
+        // this.setState({ username: evt.target.value });
+        this.setState({ [evt.target.name]: evt.target.value });
+        // if the email changes then [evt.target.name] means email
+        // name is included in <input below and must match the name in the state
+        // e.g. <input name="email">
+        // password updates, then evt.target.name means password
+        // same for username- anything that is in the state at the beginning
+    } // this is an event so we need to bind it
+
+    handleSubmit(evt) {
+        evt.preventDefault(); // stops page refreshing
+        alert(`You typed: ${this.state.username}`);
+        this.setState({ username: "" });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Form Demo</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        name="username" // name neeeds to match what we did in the state
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type="email" 
+                        name="email"
+                        placeholder="email" 
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type="password" 
+                        name="password"
+                        placeholder="password" 
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Submit!</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Form;
+

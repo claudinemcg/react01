58192474baa591803588035b4a5a9cdb784994f4
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-12-02 11:57:26 +0000

    added uuid in for unique ids

--- 13_React_Forms/shopping/shopping/src/ShoppingList.js
+++ 13_React_Forms/shopping/shopping/src/ShoppingList.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react'
 import ShoppingListForm from './ShoppingListForm';
+import { v4 as uuidv4 } from 'uuid';
 
 class ShoppingList extends Component {
     constructor(props){
         super(props)
         this.state = {
             items: [
-                {name: "Milk", qty: "2 litres"},
-                {name: "Eggs", qty: "1 pack"}
+                {name: "Milk", qty: "2 litre", id: uuidv4()},
+                { name: "Eggs", qty: "1 pack", id: uuidv4()}
             ]
         };
         this.addItem = this.addItem.bind(this);
     }
     
     addItem(item) { // adds item object e.g. {name:"...", qty:""}
+        let newItem = {...item, id: uuidv4()}
         this.setState(state => ({ //state is the old state
-            items: [...state.items, item] // old items in state, new item
+            items: [...state.items, newItem] // old items in state, new item
         }))
     }
     renderItems(){ //method for rendering items
         return(
             <ul>
                 {this.state.items.map(item =>(
-                  <li>
+                  <li key={item.id}>
                     {item.name}: {item.qty}
                   </li>  
                 ))}

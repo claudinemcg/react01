7c27cdecce45c731ac0b8ab3ddae07a4dde27f0b
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-12-13 17:11:07 +0000

    added option to strikethrough a completed task

--- 13_React_Forms/todo2/src/Todo.js
+++ 13_React_Forms/todo2/src/Todo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Todo.css'
 
 class Todo extends Component {
     constructor(props) {
@@ -11,6 +12,7 @@ class Todo extends Component {
         this.toggleForm = this.toggleForm.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleUpdate = this.handleUpdate.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
     }
 
     toggleForm(){
@@ -34,6 +36,9 @@ class Todo extends Component {
         })
     }
     
+    handleToggle() {
+        this.props.toggleTodo(this.props.id)
+    }
     render() {
         let result;
         if (this.state.isEditing){
@@ -54,7 +59,8 @@ class Todo extends Component {
         } else {
             result = (
                 <div>
-                    <li>{this.props.task}</li>
+                    <li className={this.props.completed ? 'completed':''} 
+                    onClick={this.handleToggle}>{this.props.task}</li>
                     <button onClick={this.toggleForm}>Edit</button>
                     <button onClick={this.handleDeleteClick}>Delete</button>
                 </div>
--- 13_React_Forms/todo2/src/TodoList.js
+++ 13_React_Forms/todo2/src/TodoList.js
@@ -9,6 +9,7 @@ class TodoList extends Component {
         this.create = this.create.bind(this);
         this.remove = this.remove.bind(this);
         this.update = this.update.bind(this);
+        this.toggleCompletion = this.toggleCompletion.bind(this);
     }
 
     create(newTask) {
@@ -33,6 +34,15 @@ class TodoList extends Component {
             this.setState({todos: updatedTodos});
     }
 
+    toggleCompletion(id) {
+        const updatedTodos = this.state.todos.map(todo => {
+            if (todo.id === id) {
+                return { ...todo, completed: !todo.completed }
+            }
+            return todo;
+        })
+        this.setState({ todos: updatedTodos });
+    }
         
     render() {
         const todos = this.state.todos.map(todo =>
@@ -40,8 +50,10 @@ class TodoList extends Component {
                 key={todo.id}
                 id={todo.id}
                 task={todo.task}
+                completed={todo.completed}
                 removeTask={this.remove}
                 updateTodo={this.update}
+                toggleTodo={this.toggleCompletion}
             />
         )
 
